0a2c14b28477b28231b76bcc7e52fccc31848b5e
marc2332 <mespinsanz@gmail.com>
2022-03-28 13:57:35 +0200

    :construction: Few changes

--- build_website.ts
+++ build_website.ts
@@ -1,6 +1,6 @@
 import { join } from "https://deno.land/std@0.122.0/path/mod.ts";
 import build from "./vale.ts";
 
-const projectPath = join(Deno.cwd(), "example");
+const projectPath = join(Deno.cwd(), "docs");
 
 await build(projectPath);
