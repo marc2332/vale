ff2332a330b65ca0a46e9a11f9bc674c87ad25e5
marc2332 <mespinsanz@gmail.com>
2022-03-29 21:34:11 +0200

    :bug: Typo

--- docs/build.ts
+++ docs/build.ts
@@ -1,5 +1,5 @@
 import { join } from "https://deno.land/std@0.122.0/path/mod.ts";
-import build from "https://deno.land/x/vale@0.1.2/vale.ts";
+import build from "../vale.ts";
 
 const projectPath = join(Deno.cwd(), "docs");
 
