2bb34ad26de819bf6f2b0ae6ed3f20b7022e9daa
marc2332 <mespinsanz@gmail.com>
2022-03-29 21:32:21 +0200

    :bug: Typos

--- docs/build.ts
+++ docs/build.ts
@@ -1,5 +1,6 @@
-import build from "../vale.ts";
+import { join } from "https://deno.land/std@0.122.0/path/mod.ts";
+import build from "https://deno.land/x/vale@0.1.2/vale.ts";
 
-const projectPath = Deno.cwd();
+const projectPath = join(Deno.cwd(), "docs");
 
 await build(projectPath);
